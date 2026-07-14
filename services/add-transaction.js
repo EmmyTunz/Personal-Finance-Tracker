5b1ae3452c05d39c04c0d6a9c6af242d8e8ee210
EmmyTunz <tummagold@gmail.com>
2026-07-14 22:41:40 +0100

    i added the add and view all transaction functionalities using js calling the endpoints from My-Wallet, tested it and it works just fine

---
+++ services/add-transaction.js
@@ -0,0 +1,28 @@
+const BASE_URL = "https://my-wallet-3a6i.onrender.com";
+async function handleSubmit(event) {
+    event.preventDefault();
+
+    const newTransaction = {
+        id: 0,
+        amount: Number(document.getElementById("amount").value),
+        type: document.getElementById("type").value,
+        category: document.getElementById("category").value,
+        date: new Date().toISOString(),
+        note: document.getElementById("note").value,
+    };
+
+    const response = await fetch(`${BASE_URL}/transaction/new`, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(newTransaction)
+    });
+
+    const statusMessage = document.getElementById("status-message");
+
+    if (response.ok) {
+        statusMessage.textContent = "Transaction added ✅";
+        document.getElementById("transaction-form").reset();
+    }
+}
+
+document.getElementById("transaction-form").addEventListener("submit", handleSubmit);
---
+++ services/view-transaction.js
@@ -0,0 +1,17 @@
+const BASE_URL = "https://my-wallet-3a6i.onrender.com"
+async function loadTransactions() {
+  const response = await fetch(`${BASE_URL}/transaction`);
+  const data = await response.json();
+
+  const transactions = data.map(item => item.Transaction);
+  const list = document.getElementById("transaction-list");
+  list.innerHTML = "";
+
+  transactions.forEach(txn => {
+    const item = document.createElement("li");
+    item.textContent = `${txn.date} - ${txn.category} - N${txn.amount} - description: ${txn.note}`;
+    list.appendChild(item);
+  });
+}
+
+loadTransactions();
